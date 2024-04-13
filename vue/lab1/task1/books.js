a2b9c87333096fc4e442e8b048362d3a277026cf
kasemsaad <kasemsaad756@gmail.com>
2024-04-13 18:52:07 +0200

    done

---
+++ vue/lab1/task1/books.js
@@ -0,0 +1,42 @@
+books = [
+    {
+        id: 100,
+        name: 'book one',
+        category: 'Category one',
+        author: 'Author one',
+        pages: 200,
+        price: 1000,
+        isbn: 'ISBN-100',
+        image: "Irish-childrens-books-e1693410021475.jpeg"
+    },
+    {
+        id: 200,
+        name: 'book Two',
+        category: 'Category Two',
+        author: 'Author Two',
+        pages: 250,
+        price: 2000,
+        isbn: 'ISBN-200',
+        image: 's-l640.jpg'
+    },
+    {
+        id: 300,
+        name: 'book three',
+        category: 'Category three',
+        author: 'Author three',
+        pages: 200,
+        price: 2500,
+        isbn: 'ISBN-300',
+        image: './7173a8decnL._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+        id: 400,
+        name: 'book four',
+        category: 'Category four',
+        author: 'Author four',
+        pages: 150,
+        price: 2000,
+        isbn: 'ISBN-50',
+        image: './s-l640.jpg'
+    },
+];
