d20b22b8eb9cf96bf7ca89fd99aea6c91798bfaf
kasemsaad <kasemsaad756@gmail.com>
2024-04-14 21:57:37 +0200

    done

--- java_labs/HandelExaption/Exaption.java
+++
@@ -1,8 +0,0 @@
-package HandelExaption;
-
-public class Exaption extends Exception {
-
-    public Exaption() {
-        super("Error");
-    }
-}
--- python/lab3/q1.py
+++
@@ -1,3 +0,0 @@
-from employee import employee
-employe = employee(1,'kasem', 7000)
-print(employe)
