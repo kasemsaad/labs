21e1903f208e3ae1db7b1cc8f1076292f0cff5de
Alkasem saad <118116111+kasemsaad@users.noreply.github.com>
2024-04-22 23:31:23 +0200

    done

---
+++ jango/day1/book/urls.py
@@ -0,0 +1,9 @@
+from book.views import *
+
+
+[
+    path('book/list', book_list,name="book_list"),
+    path('book/add', book_add, name="book_add"),
+    path('book/add', book_add, name="book_add"),
+
+]
